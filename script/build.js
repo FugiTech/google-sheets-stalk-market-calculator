e17b9b354e10a03f5ce66fea6e95f25779d3b69e
Tom Richards <tom@tomrichards.net>
2020-04-12 15:11:24 -0400

    Add build script

---
+++ script/build.js
@@ -0,0 +1,7 @@
+const { readFileSync, writeFileSync } = require('fs')
+const ts2gas = require('ts2gas');
+
+let source = readFileSync('Code.ts').toString();
+let transpiled = ts2gas(source);
+
+writeFileSync('build/Code.js', transpiled);
